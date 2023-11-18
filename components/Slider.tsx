1d80b8769f0ecd6e17c29aae19251b6b2fca5470
Mohd Kashan Yunus <mohdkashan21165@gmail.com>
2023-11-18 18:02:56 +0530

    created slider for player

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -1,11 +1,23 @@
 "use client"
 
+import * as RadixSlider from "@radix-ui/react-slider";
 
-const Slider = () => {
+interface SliderProps {
+    value ?: number;
+    onChange ?: (value :number) => void;
+}
+
+const Slider : React.FC<SliderProps> = ({value = 1,onChange}) => {
+
+    const handleChange = (newValue: number[]) => {
+        onChange?.(newValue[0]); 
+    }
     return (
-        <div>
-            Slider
-        </div>
+        <RadixSlider.Root className="relative flex items-center select-none touch-none w-full h-10" defaultValue={[1]} value={[value]} max={1} step={0.1} aria-label="Volume">
+            <RadixSlider.Track className="bg-neutral-600 relative grow rounded-full h-[3px]">
+                <RadixSlider.Range className="absolute bg-white rounded-full h-full5"/>
+            </RadixSlider.Track>
+        </RadixSlider.Root>
     )
 }
 
