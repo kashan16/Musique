2b46d2041a8d849fc4af7c23a4b13bf58ab203a2
Mohd Kashan Yunus <mohdkashan21165@gmail.com>
2023-11-21 18:55:26 +0530

    Account Page created

---
+++ app/account/component/AccountContent.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+import Button from "@/components/Button";
+import { useUser } from "@/hooks/useUser";
+
+const AccountContent = () => {
+  const router = useRouter();
+  const { isLoading, subscription, user } = useUser();
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (!isLoading && !user) {
+      router.replace('/');
+    }
+  }, [isLoading, user, router]);
+
+  const redirectToCustomerPortal = async () => {
+    setLoading(true);
+  };
+
+  return ( 
+    <div className="mb-7 px-6">
+      {!subscription && (
+        <div className="flex flex-col gap-y-4">
+        <p>No active plan.</p>
+        <Button 
+          onClick={() => {}}
+          className="w-[300px]"
+        >
+          Subscribe
+        </Button>
+      </div>
+      )}
+      {subscription && (
+        <div className="flex flex-col gap-y-4">
+          <p>You are currently on the 
+            <b> {subscription?.prices?.products?.name} </b> 
+            plan.
+          </p>
+          <Button
+            disabled={loading || isLoading}
+            onClick={redirectToCustomerPortal}
+            className="w-[300px]"
+          >
+            Open customer portal
+          </Button>
+        </div>
+      )}
+    </div>
+  );
+}
+ 
+export default AccountContent;
