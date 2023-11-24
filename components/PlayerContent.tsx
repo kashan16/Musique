14545340f2dccade42934c80eebb9e86e0eee492
Mohd Kashan Yunus <mohdkashan21165@gmail.com>
2023-11-24 19:43:31 +0530

    Seekbar

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -110,15 +110,15 @@ const PlayerContent : React.FC<PlayerContentProps> = ({song , songUrl}) => {
             </div>
             <div className="flex md:hidden col-auto w-full justify-end items-center">
                 <div onClick={handlePlay} className="h-10 w-10 flex items-center justify-center rounded-full bg-white p-1 cursor-pointer">
-                    <Icon size={30} className="text-black"/>
+                    <Icon size={20} className="text-black"/>
                 </div>
             </div>
             <div className="hidden md:flex h-full justify-center items-center w-full max-w-[722px] gap-x-6">
-                <AiFillStepBackward onClick={onPlayPrevious} size={30} className="text-neutral-400 cursor-pointer hover:text-white transition"/>
+                <AiFillStepBackward onClick={onPlayPrevious} size={28} className="text-neutral-400 cursor-pointer hover:text-white transition"/>
                     <div onClick={handlePlay} className="flex items-center justify-center h-10 w-10 rounded-full bg-white p-1 cursor-pointer">
                         <Icon size={30} className="text-black" />
                     </div>
-                <AiFillStepForward onClick={onPlayNext} size={30} className="text-neutral-400 cursor-pointer hover:text-white transition"/>
+                <AiFillStepForward onClick={onPlayNext} size={28} className="text-neutral-400 cursor-pointer hover:text-white transition"/>
             </div>
             <div className="hidden md:flex w-full justify-end items-center gap-x-2">
                     <VolumeIcon onClick={toggleMute} className="cursor-pointer" size={40}/>
--- components/SeekBar.tsx
+++ components/SeekBar.tsx
@@ -1,6 +1,6 @@
 import { Song } from "@/types";
 import * as RadixSlider from "@radix-ui/react-slider";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface SeekbarProps {
   value?: number;
@@ -9,31 +9,53 @@ interface SeekbarProps {
   data: Song;
 }
 
-const SeekBar: React.FC<SeekbarProps> = ({ value = 0, onChange, duration = 0, data }) => {
+const SeekBar: React.FC<SeekbarProps> = ({ value = 0, onChange, data }) => {
   const [songDuration, setSongDuration] = useState<number | null>(null);
 
+  useEffect(() => {
+    const fetchSongDuration = async () => {
+      try {
+        // Fetch song duration and update state
+        const duration = await getSongDuration(data);
+        setSongDuration(duration);
+      } catch (error) {
+        console.error('Error fetching song duration:', error);
+      }
+    };
+
+    fetchSongDuration();
+  }, [data]);
+
   const handleChange = (newValue: number[]) => {
+    // Update the displayed duration when the seek bar value changes
+    const newDuration = newValue[0] * (songDuration || 0);
+    setSongDuration(newDuration);
+
+    // Call the provided onChange prop
     onChange?.(newValue[0]);
   };
 
   return (
-    <div style={{ position: 'relative', width: '256px' }}>
-      <RadixSlider.Root className="relative flex items-center select-none touch-none h-full" value={[value]} onValueChange={handleChange} aria-label="Volume">
-        {songDuration !== null && <div>
-          {formatDuration(songDuration)}
-        </div>}
-        <RadixSlider.Track className="bg-neutral-600 relative grow rounded-full h-[3px]">
-          <RadixSlider.Range className="absolute bg-white rounded-full h-full transition-all" style={{ transform: `scaleX(${value})` }} />
-        </RadixSlider.Track>
-      </RadixSlider.Root>
-    </div>
+  <div style={{ position: 'relative', width: '256px' , marginTop: '-10px'}}>
+  <RadixSlider.Root className="relative flex items-center select-none touch-none h-full" value={[value]} onValueChange={handleChange} aria-label="Volume" max={1}>
+  <div style={{ marginRight: '10px' }} className="text-white text-sm">0:00</div>
+    <RadixSlider.Track className="bg-neutral-600 relative grow rounded-full h-[3px]">
+      <RadixSlider.Range className="absolute bg-white rounded-full h-full transition-all" style={{ transform: `scaleX(${songDuration})` }} />
+    </RadixSlider.Track>
+    <div style={{ marginLeft: '10px' }} className="text-white text-sm">{formatDuration(songDuration || 137)}</div>
+  </RadixSlider.Root>
+</div>
   );
 };
 
 const formatDuration = (duration: number): string => {
   const minutes = Math.floor(duration / 60);
-  const seconds = duration % 60;
+  const seconds = Math.floor(duration % 60);
   return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+};
+
+const getSongDuration = async (data: Song): Promise<number> => {
+  return 137;
 };
 
 export default SeekBar;
